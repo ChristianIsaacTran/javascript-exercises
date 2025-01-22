9343bdd5eaf91af6decd0dc5662e9504847f7c34
ChristianIsaacTran <70768038+ChristianIsaacTran@users.noreply.github.com>
2025-01-22 17:42:28 -0600

    Finished 05-sumAll
    Did not look at the solution yet, but my solution
    involves manually checking for specific
    cases outlined by the test cases, and then
    performing the normal adding operations with a
    for loop.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if(start > end){
+        let startTemp = start;
+        let endTemp = end;
 
+        end = startTemp;
+        start = endTemp;
+    }
+    if(start < 0 || end < 0 || typeof start != "number" || typeof end != "number" || !Number.isInteger(start) || !Number.isInteger(end)){
+        return "ERROR";
+    }
+
+    let total = 0;
+
+    for(let i = start; i <= end; i++){
+        total += i;
+    }
+
+    return total;
 };
 
 // Do not edit below this line
