2d94d4dea8d2da3da5db2e13f6334b4a0e6be3a6
ChristianIsaacTran <70768038+ChristianIsaacTran@users.noreply.github.com>
2025-01-22 16:10:23 -0600

    Finished 02-repeatString
    I used a similar for loop with conditional
    statements to pass the tests to check if
    the repeat number is lower than 0. The solution
    used less logic checking which increases
    performance.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function (word, repeatNum) {
 
+    let inputWord = "";
+
+    if (repeatNum > 0) {
+        inputWord = word;
+        for (let i = 1; i < repeatNum; i++) {
+            inputWord += word;
+        }
+    }
+    else if(repeatNum < 0){
+        return "ERROR"
+    }
+
+    return inputWord;
 };
 
 // Do not edit below this line
