6bce7ecc67a5f3ef1dab744ce6bba5e96744974c
ChristianIsaacTran <70768038+ChristianIsaacTran@users.noreply.github.com>
2025-01-22 16:32:11 -0600

    Finished 03-reverseString
    Reversed string by splitting the string by
    a empty pattern which causes all characters
    to be put into an array, then used the
    built in array "reverse()" method to reverse
    the entire order of the array, then put it
    back together with the built in "join()" on an
    empty pattern "" to join it all back together
    into a string.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
+const reverseString = function(inputWord) {
+    
+    let wordArray = inputWord.split("");
+    let reversedWordArray = wordArray.reverse();
+    let reversedWord = reversedWordArray.join("");
+
+    return reversedWord;
 
 };
 
 // Do not edit below this line
 module.exports = reverseString;
+
