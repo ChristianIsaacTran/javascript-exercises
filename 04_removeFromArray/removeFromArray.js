b541ebe3846c13bb265679a94be777adec09d486
ChristianIsaacTran <70768038+ChristianIsaacTran@users.noreply.github.com>
2025-01-22 17:26:25 -0600

    Finished 04-removeFromArray
    Using the same concept as the solution, you are
    given an array and another array of arguments
    to "remove" or exclude from the given array. By
    continuously checking if the given number is
    on that removal list, we can push all the
    elements that aren't on that list of numbers to
    remove to create an array without those numbers.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function (numArray, ...removeArray) {
+    // The second parameter is an array of indenfinite arguments written in rest parameter format. Works just like an array.
+    let changedArray = [];
+
+    //Note, using for...of is different from for...in
+    //Use for...of to iterate through the objects
+    for(const num of numArray){
+        if(!(removeArray.includes(num))){
+            changedArray.push(num);
+        }
+    }
+
+    return changedArray;
 };
 
 // Do not edit below this line
